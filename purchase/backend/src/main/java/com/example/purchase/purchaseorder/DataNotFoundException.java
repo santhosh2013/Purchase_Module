b6c1f373398ce808d7e01b86571bef31375aef59
K, Abinash (A.) <ak82@ford.com>
2025-10-28 18:18:09 +0530

    dummy commit

---
+++ purchase/backend/src/main/java/com/example/purchase/purchaseorder/DataNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.purchase.purchaseorder;
+
+public class DataNotFoundException extends Exception{
+    public DataNotFoundException(String Message) {
+        super(Message);
+    }
+}
