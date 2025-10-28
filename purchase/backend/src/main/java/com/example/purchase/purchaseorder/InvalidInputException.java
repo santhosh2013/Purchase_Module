a9f1d8a473aff6e8404cd6806a23879f92e1d291
K, Abinash (A.) <ak82@ford.com>
2025-10-28 18:22:01 +0530

    dummy commit

---
+++ purchase/backend/src/main/java/com/example/purchase/purchaseorder/InvalidInputException.java
@@ -0,0 +1,7 @@
+package com.example.purchase.purchaseorder;
+
+public class InvalidInputException extends Exception{
+    public InvalidInputException(String Message) {
+        super(Message);
+    }
+}
